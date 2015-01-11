5553a156aec1e380df247e79ebfa28387e55486e
Kosuke Arisawa <arisawa@gmail.com>
2015-01-11 18:28:11 +0900

    Re-created

--- rails-template.rb
+++ rails-template.rb
@@ -1,67 +1,48 @@
-gem "therubyracer", platforms: :ruby
-gem "unicorn"
-gem "maruku"
-gem "iconv"
-gem "twitter-bootstrap-rails", git: "https://github.com/seyhunak/twitter-bootstrap-rails.git", branch: "bootstrap3"
-
-gem_group :deploy do
-  gem "capistrano"
-  gem "capistrano-rbenv"
-  gem "capistrano-bundler"
-  gem "capistrano-rails"
-end
-
-gem_group :development, :test do
-  gem "rspec-rails", "~> 3.0"
-  gem "guard-rails"
-  gem "guard-bundler"
-  gem "guard-rspec"
-  gem "terminal-notifier-guard"
-  gem "quiet_assets"
-  gem "rails-erd"
-end
+gem 'slim'
+gem 'simple_form', git: 'https://github.com/plataformatec/simple_form'
 
 gem_group :development do
-  gem "pry-rails"
-  gem "better_errors"
-  gem "binding_of_caller"
-  gem "meta_request"
-  gem "pry-byebug"
-  gem "pry-stack_explorer"
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'hirb'
+  gem 'hirb-unicode'
+  gem 'pry-rails'
+  gem 'pry-doc'
+  gem 'pry-byebug'
+  gem 'pry-stack_explorer'
+  gem 'quiet_assets'
+  gem 'rack-mini-profiler'
+  gem 'tapp'
+  gem 'view_source_map'
+  gem 'rspec-rails'
+  gem 'guard-rails'
+  gem 'guard-rspec'
+  gem 'guard-bundler'
+  gem 'terminal-notifier-guard'
 end
 
 gem_group :test do
-  gem "webmock"
-  gem "factory_girl_rails", "~> 4.0"
-  gem "timecop"
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+  gem 'database_cleaner'
 end
 
-run "bundle install"
+run "cat << EOF >> .gitignore
+/.bundle
+/log
+/tmp
+/.vagrant
+*.swp
+*~
+.DS_Store
+/db/*.sqlite3
+/db/*.sqlite3-journal
+EOF"
 
-file 'Guardfile', <<-EOF
-guard :bundler do
-  watch('Gemfile')
+after_bundle do
+  run 'bundle exec guard init rspec'
+  run 'bundle binstub rails'
+  git :init
+  git add: "."
+  git commit: %Q{ -m 'Initial commit' }
 end
-
-guard 'rails' do
-  watch('Gemfile.lock')
-  watch(%r{^(config|lib)/.*})
-end
-
-guard :rspec, cmd: 'spring rspec -f doc' do
-  watch(%r{^spec/.+_spec\.rb$})
-  watch(%r{^lib/(.+)\.rb$})     { |m| "spec/lib/#{m[1]}_spec.rb" }
-  watch('spec/spec_helper.rb')  { 'spec' }
-
-  watch(%r{^app/(.+)\.rb$})                           { |m| "spec/#{m[1]}_spec.rb" }
-  watch(%r{^app/(.*)(\.erb|\.haml)$})                 { |m| "spec/#{m[1]}#{m[2]}_spec.rb" }
-  watch(%r{^spec/factories/(.+)\.rb$})                { 'spec/factories_spec.rb' }
-  watch(%r{^spec/support/(.+)\.rb$})                  { 'spec' }
-  watch('config/routes.rb')                           { 'spec/routing' }
-  watch('app/controllers/application_controller.rb')  { 'spec/controllers' }
-end
-EOF
-
-git :init
-git add: "."
-git commit: "-m 'rails new'"
